e708d8d8e72c7587c60c814dfc7889f7a7072ed9
zeruhur <51272598+zeruhur@users.noreply.github.com>
2024-01-30 11:22:37 +0100

    sidebar update

--- docs/create_sidebar.py
+++ docs/create_sidebar.py
@@ -1,12 +1,20 @@
 import os
+
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
 sidebar_file = open('_sidebar.md', 'w')
 
 for file in files:
-	if ".md" in file:
-		name = file.split(".md")
-		file = file.replace(" ", "%20")
-		sidebar_file.write( f"* [{name[0]}]({file})\n" )
+    if ".md" in file:
+        # Rimuovere l'estensione .md e sostituire gli underscore con spazi
+        name = file.replace('_', ' ').split(".md")[0]
+
+        # Capitalizzare ogni parola
+        name = name.title()
+
+        # Sostituire gli spazi con %20 nel nome del file per l'URL
+        file_url = file.replace(" ", "%20")
+
+        sidebar_file.write(f"* [{name}]({file_url})\n")
 
 sidebar_file.close()
